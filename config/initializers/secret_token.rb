7144d796fd27cfd99631c0c1aff56cea0200b24e
FC3 <FC3@mail.ru>
2012-06-24 18:47:00 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9579424b9285585321fbf12a1bc6ec34ab377fefe52b118876ec6487530ad9f38f347b9016ef8a87b4d33fe665a9b9a59fe1b671d8c2dd42e358c1eb7f406b7b'
